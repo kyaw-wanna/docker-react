56d6387345ab5a364a206ec8ae30cf37fbcf9e79
kyawwanna2019 <kyawwanna2019@hotmail.com>
2019-07-02 17:14:00 +0700

    Change App.js in feature branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit a <code>src/App.js</code> and save to reload, this is testing.
+          CHANGE ON THE FEATURE BRANCH
         </p>
         <a
           className="App-link"
