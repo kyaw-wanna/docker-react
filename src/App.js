6cf754fb21d1c26f9cf55f1bfdb9485144a7a5a6
kyawwanna2019 <kyawwanna2019@hotmail.com>
2019-07-02 18:56:18 +0700

    Change again in APP.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit a <code>src/App.js</code> and save to reload, this is testing.
-          CHANGE ON THE FEATURE BRANCH
+          CHANGE ON THE FEATURE BRANCH, ANOTHER one
         </p>
         <a
           className="App-link"
